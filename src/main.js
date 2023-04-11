47a08430af5a404e2228d9cde22b4f5d85a50c2f
hmmini <pak4184@naver.com>
2023-04-12 00:21:23 +0900

    router 설치

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: {
+
+    }
+})
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
