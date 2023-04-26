3c85e9aa11698c86ae29edc3a8dbf361c754931c
hmmini <pak4184@naver.com>
2023-04-26 22:28:32 +0900

    - UserProfile.vue 개발

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <section>
+      <div class="user-container">
+        <div>
+          <i class="fa-solid fa-user"></i>
+        </div>
+        <div class="user-description">
+          <div v-if="getInfoItem.user">
+            {{ getInfoItem.id }}
+          </div>
+          <router-link
+              v-else
+              :to="`/user/${getInfoItem.user}`">
+            {{ getInfoItem.id }}
+          </router-link>
+          <div class="time">
+            {{ getInfoItem.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2>{{ getInfoItem.title }}</h2>
+    </section>
+    <section>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['getInfoItem'])
+  },
+  created() {
+    const itemId = this.$route.params.id;
+    this.$store.dispatch('FETCH_ITEMINFO', itemId)
+  }
+}
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 10px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,57 +1,16 @@
 <template>
-  <div>
-    <section>
-      <!-- 질문 상세 정보 -->
-      <div class="user-container">
-        <div>
-          <i class="fa-solid fa-user"></i>
-        </div>
-        <div class="user-description">
-          <router-link :to="`/user/${getInfoItem.user}`">
-            {{ getInfoItem.user }}
-          </router-link>
-          <div class="time">
-            {{ getInfoItem.time_ago }}
-          </div>
-        </div>
-      </div>
-      <h2>{{ getInfoItem.title }}</h2>
-    </section>
-    <section>
-      <!--   질문 댓글  -->
-    </section>
-<!--    <p>{{ getInfoItem.title }}</p>-->
-<!--    <p>{{ getInfoItem.content }}</p>-->
-  </div>
+  <UserProfile></UserProfile>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import UserProfile from "@/components/UserProfile.vue";
 
 export default {
-  computed: {
-    ...mapGetters(['getInfoItem'])
-  },
-  created() {
-    const itemId = this.$route.params.id;
-    this.$store.dispatch('FETCH_ITEMINFO', itemId)
+  components: {
+    UserProfile
   }
 }
 </script>
 
 <style scoped>
-.user-container {
-  display: flex;
-  align-items: center;
-  padding: 1rem;
-}
-.fa-user {
-  font-size: 2.5rem;
-}
-.user-description {
-  padding-left: 10px;
-}
-.time {
-  font-size: 0.7rem;
-}
 </style>
