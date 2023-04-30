e742f91f35d796b712d199483cf2ef67a835faf7
hmmini <pak4184@naver.com>
2023-04-30 14:29:17 +0900

    - Ask, Job, News 공통화 및 Spinner.vue 생성

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -8,6 +8,9 @@ import ListItem from '../components/ListItem.vue'
 export default {
   components: {
     ListItem
+  },
+  created() {
+    this.$store.dispatch('FETCH_ASKS');
   }
 }
 </script>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -8,34 +8,9 @@ import ListItem from '../components/ListItem.vue'
 export default {
   components: {
     ListItem
+  },
+  created() {
+    this.$store.dispatch('FETCH_JOBS');
   }
 }
 </script>
-
-<style scoped>
-.jobs-list {
-  margin: 0;
-  padding: 0;
-}
-.post {
-  align-items: center;
-  display: flex;
-  list-style: none;
-  border-bottom: solid 1px #eee;
-}
-.points {
-  color: #42B883;
-  justify-content: center;
-  height: 60px;
-  width: 80px;
-  display: flex;
-  align-items: center;
-}
-.job-title {
-  margin: 0;
-  padding: 0;
-}
-.job-text {
-  color: #828282
-}
-</style>
