dce9ec8c28fb7e676378c3d6cd6ad3719d48f160
hmmini <pak4184@naver.com>
2023-05-06 00:03:07 +0900

    - router 네비게이션을 이용한 데이터 호출 (beforeEnter)

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,17 +5,34 @@ import createListView from "@/views/CreateListView";
 import NewsView from "@/components/NewsView.vue";
 import AsksView from "@/components/AsksView.vue";
 import JosView from "@/components/JosView.vue";
+import store from "@/store/index";
 
 const routes = [
     {
         path: '/',
-        redirect: '/news'
+        redirect: '/news',
     },
     {
         path: '/news',
         name: 'news',
         // component: createListView('NewsView')
-        component: NewsView
+        component: NewsView,
+        beforeEnter: (to, from, next) => {
+            console.log('to', to);
+            console.log('from', from);
+            console.log('enter', next);
+            store.dispatch('SET_LOADINGSTATUS', true)
+                .then(() => {
+                    console.log('fetch');
+                    store.dispatch('FETCH_LIST', to.name)
+                        .then(() => {
+                            setTimeout(() =>
+                                store.dispatch('SET_LOADINGSTATUS', false), 300
+                            )
+                            next();
+                        })
+                })
+        }
     },
     {
         path: '/asks',
