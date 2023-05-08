3323f58ab4a45ad2a2d8367f3b9a8bfcd36e2e66
hmmini <pak4184@naver.com>
2023-05-09 00:34:24 +0900

    - 비동기 함수 전체 promise -> async & await로 변경

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,38 +1,50 @@
 import {fetchUserInfo, fetchItemInfo, fetchList} from "@/api";
 
 export default {
-    FETCH_USERINFO({ commit }, userName) {
-        return fetchUserInfo(userName)
-            .then(response => {
-                console.log('response', response);
-                commit('SET_USERINFO', response.data);
-            })
-            .catch(error => console.log('error', error))
-    },
-    FETCH_ITEMINFO({ commit }, itemId) {
-        return fetchItemInfo(itemId)
-            .then(response => {
-                console.log('response', response);
-                commit('SET_ITEMINFO', response.data);
-            })
-            .catch(error => {
-                console.log('error', error);
-            })
-    },
-    SET_LOADINGSTATUS({ commit }, loadingStatus) {
-        return commit('SET_LOADINGSTATUS', loadingStatus);
-    },
     //promise
     // FETCH_LIST({ commit }, pageName) {
     //     return fetchList(pageName)
     //         .then(({ data }) => commit('SET_LIST', data))
     //         .catch(error => console.log('error', error));
     // },
+    // FETCH_USERINFO({ commit }, userName) {
+    //     return fetchUserInfo(userName)
+    //         .then(response => {
+    //             console.log('response', response);
+    //             commit('SET_USERINFO', response.data);
+    //         })
+    //         .catch(error => console.log('error', error))
+    // },
+    // FETCH_ITEMINFO({ commit }, itemId) {
+    //     return fetchItemInfo(itemId)
+    //         .then(response => {
+    //             console.log('response', response);
+    //             commit('SET_ITEMINFO', response.data);
+    //         })
+    //         .catch(error => {
+    //             console.log('error', error);
+    //         })
+    // },
+    SET_LOADINGSTATUS({ commit }, loadingStatus) {
+        return commit('SET_LOADINGSTATUS', loadingStatus);
+    },
     //async
     async FETCH_LIST({ commit }, pageName) {
         const response = await fetchList(pageName);
-        console.log('async response', response.data);
+        console.log('async & await response', response.data);
         commit('SET_LIST', response.data);
+        return response;
+    },
+    async FETCH_USERINFO({ commit }, userName) {
+        const response = await fetchUserInfo(userName);
+        console.log('async & await response', response.data);
+        commit('SET_USERINFO', response.data);
+        return response;
+    },
+    async FETCH_ITEMINFO({ commit }, itemId) {
+        const response = await fetchItemInfo(itemId);
+        console.log('async & await response', response.data);
+        commit('SET_ITEMINFO', response.data);
         return response;
     }
 }
