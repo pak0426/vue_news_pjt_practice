ac4b1a02b652694f4bd3fdea3b8dd63386b6fcac
hmmini <pak4184@naver.com>
2023-05-11 19:52:57 +0900

    -make Barchart Component

--- src/components/ChartView.vue
+++ src/components/ChartView.vue
@@ -1,37 +1,15 @@
 <template>
-  <div style="width: 800px;height: 800px;">
-    <canvas id="myChart"></canvas>
-  </div>
+  <BarChart></BarChart>
 </template>
 
 <script>
-import { Chart, registerables } from "chart.js";
-Chart.register(...registerables);
+import BarChart from "@/components/BarChart.vue";
 
 export default {
-  mounted() {
-    const ctx = document.getElementById('myChart');
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
+  components: {
+    BarChart
   }
 }
-
 </script>
 
 <style scoped>
