9c4c3b3cee40412032c545dbc6f6918987d75e94
hmmini <pak4184@naver.com>
2023-05-12 00:05:37 +0900

    develop LineChart Component

--- src/components/ChartView.vue
+++ src/components/ChartView.vue
@@ -1,12 +1,17 @@
 <template>
-  <BarChart></BarChart>
+  <div>
+<!--    <BarChart></BarChart>-->
+    <LineChart></LineChart>
+  </div>
 </template>
 
 <script>
 import BarChart from "@/components/BarChart.vue";
+import LineChart from "@/components/LineChart.vue";
 
 export default {
   components: {
+    LineChart,
     BarChart
   }
 }
---
+++ src/components/LineChart.vue
@@ -0,0 +1,62 @@
+<template>
+  <div style="width: 800px;height: 800px;">
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
+
+<script>
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+
+export default {
+  data() {
+    return {
+      data: {
+        DATA_COUNT: 7,
+        NUMBER_CFG: {
+          count: this.DATA_COUNT,
+          min: -100, max: 100
+        },
+        labels: ['January', 'February', 'April', 'May', 'Jun', 'July', 'August'],
+        datasets: [
+          {
+            label: 'Dataset 1',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            borderColor: 'rgb(128,192,75)',
+            backgroundColor: 'rgb(128,192,75)',
+          },
+          {
+            label: 'Dataset 2',
+            data: [35, 29, 10, 16, 73, 101, 30],
+            borderColor: 'rgb(192,75,173)',
+            backgroundColor: 'rgb(192,75,173)',
+          }
+        ]
+      }
+    }
+  },
+  mounted() {
+    const ctx = document.getElementById('myChart');
+    new Chart(ctx, {
+      type: 'line',
+      data: this.data,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Line Chart'
+          }
+        }
+      },
+    });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
