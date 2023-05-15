3ae40b0bfa7a7ceb53ca9b270acd28c40cd5b308
hmmini <pak4184@naver.com>
2023-05-15 23:37:43 +0900

    - Barchart 수정

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -6,22 +6,57 @@
 
 <script>
 export default {
+  data() {
+    return {
+      DATA_COUNT: 7,
+      labels: ['January', 'February', 'April', 'May', 'Jun', 'July', 'August'],
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            label: 'Fully Rounded',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            borderColor: 'rgba(255, 99, 132, 0.2)',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderWidth: 3,
+            borderRadius: 1,
+            borderSkipped: false,
+          },
+          {
+            label: 'Small Radius',
+            data: [12, 19, 3, 5, 2, 3],
+            borderColor: '#0054FF',
+            backgroundColor: '#0054FF',
+            borderWidth: 3,
+            borderRadius: 5,
+            borderSkipped: false,
+          }
+        ]
+      }
+    }
+  },
   mounted() {
     const ctx = this.$refs.barChart;
     new this.$_Chart(ctx, {
       type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
-      },
+      // data: {
+      //   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      //   datasets: [{
+      //     label: '# of Votes',
+      //     data: [12, 19, 3, 5, 2, 3],
+      //     borderWidth: 1
+      //   }]
+      // },
+      data: this.data,
       options: {
-        scales: {
-          y: {
-            beginAtZero: true
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Bar Chart'
           }
         }
       }
