a8afe8e6b0a1071044a3e149c165b7334424ad63
hmmini <pak4184@naver.com>
2023-05-20 22:16:30 +0900

    add chart css

--- src/components/ChartView.vue
+++ src/components/ChartView.vue
@@ -1,10 +1,13 @@
 <template>
-  <select v-model="pick">
-    <option value="barChart">Bar Chart</option>
-    <option value="lineChart">Line Chart</option>
-    <option value="polarChart">Polar Area Chart</option>
-    <option value="radarChart">Radar Chart</option>
-  </select>
+  <div class="chartTitle">
+    <h3>Please Select Chart</h3>
+    <select v-model="pick" class="selectChart">
+      <option value="barChart">Bar Chart</option>
+      <option value="lineChart">Line Chart</option>
+      <option value="polarChart">Polar Area Chart</option>
+      <option value="radarChart">Radar Chart</option>
+    </select>
+  </div>
   <div>
     <template v-if="this.pick == 'barChart'">
       <BarChart></BarChart>
@@ -43,5 +46,15 @@ export default {
 </script>
 
 <style scoped>
+.chartTitle {
+  display: flex;
+  align-items: center;
+  gap: 13px;
+}
 
+.selectChart {
+  padding: 10px 5px;
+  border: 2px solid green;
+
+}
 </style>
